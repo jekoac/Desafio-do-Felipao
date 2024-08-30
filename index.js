0afeda0c7c35c86b50554d8cc72ed4c159cc7164
Jefferson <user@MacBook-Air-de-User.local>
2024-08-30 17:20:13 -0300

    subindo o arquivo

---
+++ index.js
@@ -0,0 +1,20 @@
+let nome = "Jefferson"; //Exemplo de nome
+let QtdExp = 100; // Exemplo de valor
+
+if (QtdExp < 1000) {
+    console.log("O Herói de nome " + nome + " está no nível Ferro");
+} else if (QtdExp <= 2000) {
+    console.log("O Herói de nome " + nome + " está no nível Bronze");
+} else if (QtdExp <= 5000) {
+    console.log("O Herói de nome " + nome + " está no nível Prata");
+} else if (QtdExp <= 7000) {
+    console.log("O Herói de nome " + nome + " está no nível Ouro");
+} else if (QtdExp <= 8000) {
+    console.log("O Herói de nome " + nome + " está no nível Platina");
+} else if (QtdExp <= 9000) {
+    console.log("O Herói de nome " + nome + " está no nível Ascendente");
+} else if (QtdExp <= 10000) {
+    console.log("O Herói de nome " + nome + " está no nível Imortal");
+} else {
+    console.log("O Herói de nome " + nome + " está no nível Radiante");
+}
